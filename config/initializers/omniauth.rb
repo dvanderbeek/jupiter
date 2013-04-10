0418d448f8b5964ea44756cb7172fc7d54f6c7e4
David Van Der Beek <earlynovrock@gmail.com>
2013-04-10 14:59:16 -0500

    FB permissions to environment variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Gon.global.FACEBOOK_APP_ID = ENV['FACEBOOK_APP_ID']
+Gon.global.FACEBOOK_PERMISSION = ENV['FACEBOOK_PERMISSION']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: 'email'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: ENV['FACEBOOK_PERMISSION']
 end
