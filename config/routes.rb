edfa8c893235655f61eaf9ca0b3164ff249aacd1
David Van Der Beek <earlynovrock@gmail.com>
2013-04-10 10:15:26 -0500

    add FB login...not working properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Jupiter::Application.routes.draw do
-  get "home/index"
 
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+  
   root to: "home#index"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
