a8584117a7c25182288d6106cb58a88363f94462
David Van Der Beek <earlynovrock@gmail.com>
2013-04-17 13:45:31 -0500

    email settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,20 @@ Jupiter::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'jupiterapp.herokuapp.com',
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+
+  config.action_mailer.default_url_options = {host: "jupiterapp.herokuapp.com"}
+
 
   # Enable threaded mode
   # config.threadsafe!
